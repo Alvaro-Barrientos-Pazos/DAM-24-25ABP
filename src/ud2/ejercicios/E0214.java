24300f9a1fa748cfb49b3cb7d1079bdbb4703f96
alvaro <barrientospazosalvaro@gmail.com>
2024-10-23 14:23:16 +0200

    finalizado

--- src/ud2/ejercicios/E0214.java
+++ src/ud2/ejercicios/E0214.java
@@ -22,50 +22,57 @@ public class E0214 {
     System.out.print("Introduce el año de la fecha: ");
     anho = sc.nextInt();
 
-    dia+=1;
+    
+    int mDia = dia+1;
+    int mMes = mes;
+    int mAnho = anho;
 
     boolean bisiesto = false;
+    
 
     if (anho%4 == 0 && anho%100 != 0 || anho%400 == 0){
         bisiesto = true;
     }
 
-    // Es un coñazo checkear otra vez por fechas invalidas sin funciones asi que solo compruebo lo basico, se quedan fuera los febreros y los meses de 30 dias. Demasiado escribir.
-    if (dia < 1 || dia > 31 || mes < 1 || mes > 12 || anho < 1 ){
+    // Es un coñazo checkear otra vez por fechas invalidas sin funciones asi que solo compruebo lo basico, se quedan fuera los meses de 30 dias. Demasiado escribir.
+    if (dia < 1 || dia > 31 || mes < 1 || mes > 12 || anho < 1 || (mes == 2 && dia>29 && bisiesto)|| (mes == 2 && dia>28)){
         System.out.println("La fecha es invalida"); 
     }
     else{
         switch (mes) {         
             case 2:
-                if (dia > 28 || (dia>29 && bisiesto)){
-                    dia = 1;
-                    mes += 1;
+                if ((mDia > 28 && !bisiesto) || (mDia>29 && bisiesto)){
+                    mDia = 1;
+                    mMes += 1;
+                    break;
                 }
-                break;
+                
 
             case 4,6,9,11:
-                if (dia > 30){
-                    dia = 1;
-                    mes += 1;
+                if (mDia > 30){
+                    mDia = 1;
+                    mMes += 1;
+                    break;
                 }
-                break;
+                
 
             case 1,3,5,7,8,10,12:
-                if (dia>31){
-                    dia = 1;
-                    mes += 1;
+                if (mDia > 31){
+                    mDia = 1;
+                    mMes += 1;
+                    break;
                 }
-                break;
-            
         }
+        
+        if (mMes > 12){
+            mAnho+=1;
+        }
+
+        System.out.printf("El día siguiente a %d / %d / %d sería: %d / %d / %d \n",dia,mes,anho,mDia,mMes,mAnho);
     }
 
-    if (mes > 12){
-        anho+=1;
-    }
     
 
   }
-
 
 }
