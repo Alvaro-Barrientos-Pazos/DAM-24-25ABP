324dfb3e94fea984c65596537f0496f815505be2
alvaro <barrientospazosalvaro@gmail.com>
2024-10-30 17:12:23 +0100

    correcciones

--- src/ud2/ejerciciosbucles/E0305.java
+++ src/ud2/ejerciciosbucles/E0305.java
@@ -17,31 +17,39 @@ import java.util.Scanner;
 public class E0305 {
     public static void main(String[] args) {
 
-        long msStart = System.currentTimeMillis(); // Hora en milisegundos del comienzo del programa
-        long msEnd;                                // Hora en milisegundos de la finalizacion del programa
+        final byte S_MINUTES   = 60;                     // Segundos en un minuto
+        final int  S_HOUR      = S_MINUTES * S_MINUTES;  // Segundos en una hora
+        final int  S_MILLISECS = 1000;
 
-        final byte MAX = 100;
+        final byte MAX = 10;
         final byte MIN = 1;
         final byte N_OPERATORS = 3;
 
-
         Scanner sc = new Scanner(System.in);
+
+        long msStart, msEnd;     // Tiempo en milisegundos de comienzo y final de la respuesta
+
+        int seconds, minutes;
 
         int nIntentos = 5;
         int nWins = 0;
         int result = -1;
+        
+        int input;
+
+        int a, b, operator;
+        char cOperator = '+';
+
 
         while (nIntentos>0){
 
-            int a = (int) (Math.random() * MAX ) + MIN;
-            int b = (int) (Math.random() * MAX ) + MIN;
-            int operator = (int) (Math.random() * N_OPERATORS) + 1;
+            a = (int) (Math.random() * MAX ) + MIN;
+            b = (int) (Math.random() * MAX ) + MIN;
+            operator = (int) (Math.random() * N_OPERATORS) + 1;
 
-            char cOperator = '+';
-
-        
             switch (operator) {
                 case 1:
+                    cOperator = '+';
                     result = a + b;
                     break;
 
@@ -56,31 +64,35 @@ public class E0305 {
                     break;
             }
 
+            msStart = System.currentTimeMillis();
+
+            
             System.out.printf("Cual es el resultado de %d %c %d?\n", a, cOperator, b);
-            int input = sc.nextInt();
+            input = sc.nextInt();
 
             msEnd = System.currentTimeMillis();
 
-
-            final int S_MINUTES = 60;    // Segundos en un minuto
-            final int S_HOUR    = S_MINUTES * S_MINUTES; // Segundos en una hora
-            
-
-            int seconds = (int)(msEnd-msStart) / 1000 ;
-            int minutes = seconds % S_HOUR/S_MINUTES;
-            int hours   = seconds / S_HOUR;
-            seconds     = seconds % S_MINUTES;
+            seconds = (int)(msEnd-msStart) / S_MILLISECS ;
+            minutes = seconds % S_HOUR/S_MINUTES;
+            seconds = seconds % S_MINUTES;
 
             if (input == result){
                 nWins++;
-
-                System.out.printf("Correcto [tiempo respuesta: %02d:%02d:%02d]\n",hours,minutes,seconds);
+                System.out.printf("¡Correcto! [tiempo respuesta: %02d:%02d]\n",minutes,seconds);
             }
             else {
-                System.out.printf("Incorrecto te quedan %d intentos [tiempo respuesta: %02d:%02d:%02d] [nºAciertos: %d]\n",nIntentos,hours,minutes,seconds,nWins);
                 nIntentos--;
+                if (nIntentos==0){
+                    System.out.printf("Has perdido... nº aciertos: %d\n",nWins);    
+                }
+                else{
+                    System.out.printf("Incorrecto la respuesta era %d. Te quedan %d intentos [tiempo respuesta: %02d:%02d]\n",result,nIntentos,minutes,seconds);
+                }
+                
             }
+
         }
+        sc.close();
     
     }
 
