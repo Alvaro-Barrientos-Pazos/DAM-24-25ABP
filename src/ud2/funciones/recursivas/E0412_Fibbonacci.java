c08ea62cd65a3324c282ea68e1398606e2fd804c
IIGreed <antropodo54@gmail.com>
2024-11-13 23:18:54 +0100

    ud2.funciones.E0412

--- src/ud2/funciones/recursivas/E0412_Fibbonacci.java
+++ src/ud2/funciones/recursivas/E0412_Fibbonacci.java
@@ -10,12 +10,15 @@ package ud2.funciones.recursivas;
 
 public class E0412_Fibbonacci {
     public static void main(String[] args) {
-        System.out.println(fibonacci(12));
+        System.out.println(fibonacci(10));
     }
 
     static int fibonacci(int n){
-        return n;
+
+        if (n < 2){
+            return n;
+        }
+
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
-
-
 }
