46236099fdd4b45c8e517d66f9c33b2b4458c404
Alvaro <antropodo54@gmail.com>
2025-01-14 16:45:29 +0100

    sinRepetidos

--- src/ud4/E0507.java
+++ src/ud4/E0507.java
@@ -4,24 +4,39 @@ import java.util.Arrays;
 
 public class E0507 {
     public static void main(String[] args) {
-
+        int[] results = sinRepetidos(new int[]{ 1,2,6,2,3,1,5 });
 
     }
 
+
     static int[] sinRepetidos(int[]a){
-        int[] t = a.clone();
+        int[] unique_arr = new int[a.length];
+        unique_arr[0] = a[0];
 
-        boolean rep
+        boolean isUnique = true;
 
-        for (int i = 0; i < a.length; i++){
-            for (int j = 0; j < t.length; j++){
+        int unique_counter = 1;
 
+        for (int i = 1; i < a.length; i++){
+
+            isUnique = true;
+
+            for (int j = 0; j < unique_counter; j++){
+                if (a[i] == unique_arr[j]){
+                    isUnique = false;
+                    break;
+                }
+                
+            }
+
+            if (isUnique){
+                unique_arr[unique_counter] = a[i];
+                unique_counter++;
             }
 
         }
 
-        
-        return a;
+        return Arrays.copyOf(unique_arr, unique_counter);
     }
 
 
