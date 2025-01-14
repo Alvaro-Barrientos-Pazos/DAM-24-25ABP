d903993556c9a26bcbd42bf026de62333fddaa7f
alvaro <barrientospazosalvaro@gmail.com>
2025-01-14 14:30:44 +0100

    worddle

---
+++ src/ud4/E0507.java
@@ -0,0 +1,28 @@
+package ud4;
+
+import java.util.Arrays;
+
+public class E0507 {
+    public static void main(String[] args) {
+
+
+    }
+
+    static int[] sinRepetidos(int[]a){
+        int[] t = a.clone();
+
+        boolean rep
+
+        for (int i = 0; i < a.length; i++){
+            for (int j = 0; j < t.length; j++){
+
+            }
+
+        }
+
+        
+        return a;
+    }
+
+
+}
