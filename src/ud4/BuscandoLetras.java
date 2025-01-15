77d9ad39b35d319c9cc0cb2259177ea6e8dadacc
Alvaro <antropodo54@gmail.com>
2025-01-15 16:26:25 +0100

    15-01-2025

---
+++ src/ud4/BuscandoLetras.java
@@ -0,0 +1,37 @@
+package ud4;
+
+import java.util.Arrays;
+
+public class BuscandoLetras {
+
+    public static void main(String[] args) {
+        String cadena = "HolaooH";
+        char testChar = 'z';
+        //testChar = 'o';
+        
+        int[]results = buscarLetra(cadena,testChar);
+
+        System.out.println(Arrays.toString(results));
+
+    }
+
+
+    public static int[] buscarLetra(String cadena, char letra){
+        
+        char[] arrChar = cadena.toCharArray();
+        int[] indexes = new int[arrChar.length];
+        int counter = 0;
+
+
+        for (int i=0;i< arrChar.length; i++) {
+            if (arrChar[i] == letra){
+                indexes[counter] = i;
+                counter++; 
+            }
+        }
+        return Arrays.copyOf(indexes, counter);
+    }
+
+
+
+}
