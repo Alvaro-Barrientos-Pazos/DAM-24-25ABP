9185e3272c674b7cd69ef34e05ec70c58b06b7db
alvaro <barrientospazosalvaro@gmail.com>
2025-01-22 14:00:42 +0100

    arr

--- src/ud4/ArraysEstadisticas/MatricesTriangulares.java
+++ src/ud4/ArraysEstadisticas/MatricesTriangulares.java
@@ -10,6 +10,26 @@ public class MatricesTriangulares {
                 return false;
             }
         }
+
+        boolean isTri1 = true;
+        boolean isTri2 = true;
+
+        for (int i = 0; i < t.length; i++){
+            for (int j = 0; j < i; j++){
+                if (t[i][j] != 0){
+                    isTri1 = false;
+                }
+            }
+        }
+
+
+        for (int i = 0; i < t.length; i++){
+            for (int j = 0; j < i; j++){
+                if (t[i][j] == 0){
+                    isTri2 = false;
+                }
+            }
+        }
         
         return true;
     }
